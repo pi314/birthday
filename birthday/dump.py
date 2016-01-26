b889136f5cd60b5e78d5ba66132da27ff4a3df9d
Cychih <michael66230@gmail.com>
2016-01-27 03:10:33 +0800

    implement completion for ``add``

--- birthday/dump.py
+++ birthday/dump.py
@@ -3,6 +3,13 @@ from .model import Birthday
 
 def command(args):
     Birthday.connect()
-    for record in Birthday.select():
-        print(record.name)
+    if args.info == 'names':
+        for record in Birthday.select():
+            print(record.name)
+
+    elif args.info == 'years':
+        for record in Birthday.select():
+            if record.year:
+                print(record.year)
+
     Birthday.disconnect()
